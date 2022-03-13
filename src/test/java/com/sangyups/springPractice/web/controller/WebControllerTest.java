8b2275731eea23ac9e2202b8c600abc27a9c9fae
Sangyups <feels4637@gmail.com>
2022-03-13 16:15:34 +0900

    test: [WebControllerTest] 파일 추가

---
+++ src/test/java/com/sangyups/springPractice/web/controller/WebControllerTest.java
@@ -0,0 +1,19 @@
+package com.sangyups.springPractice.web.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class WebControllerTest {
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+}
