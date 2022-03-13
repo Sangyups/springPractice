660d58ac26fd0d5af379f6751163d8f72a40ae4e
Sangyups <feels4637@gmail.com>
2022-03-13 16:38:48 +0900

    test: [WebControllerTest] 메인페이지 테스트

--- src/test/java/com/sangyups/springPractice/web/controller/WebControllerTest.java
+++ src/test/java/com/sangyups/springPractice/web/controller/WebControllerTest.java
@@ -1,8 +1,10 @@
 package com.sangyups.springPractice.web.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,4 +18,12 @@ class WebControllerTest {
   @Autowired
   private TestRestTemplate restTemplate;
 
+  @Test
+  public void 메인페이지() {
+    // when
+    String body = restTemplate.getForObject("/", String.class);
+
+    // then
+    assertThat(body).contains("스프링부트를 사용한 웹 서비스");
+  }
 }
