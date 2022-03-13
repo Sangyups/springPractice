1b84c65ef623c02dbc8b8f705a829bd4f3711909
Sangyups <feels4637@gmail.com>
2022-03-13 18:22:32 +0900

    feat: [PostsService] repository에서 가져온 최신 순 리스트를 dto로 변환해서 반환

--- src/main/java/com/sangyups/springPractice/posts/service/PostsService.java
+++ src/main/java/com/sangyups/springPractice/posts/service/PostsService.java
@@ -1,19 +1,29 @@
 package com.sangyups.springPractice.posts.service;
 
 import com.sangyups.springPractice.posts.domain.dto.PostsRequestDto;
+import com.sangyups.springPractice.posts.domain.dto.PostsResponseDto;
 import com.sangyups.springPractice.posts.repository.PostsRepository;
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PostsService {
 
   private final PostsRepository postsRepository;
 
-  @Transactional
   public Long save(PostsRequestDto dto) {
     return postsRepository.save(dto.toEntity()).getId();
+  }
+
+  @Transactional(readOnly = true)
+  public List<PostsResponseDto> getPostsByRecent() {
+    return postsRepository.findAllByDesc().stream()
+        .map(PostsResponseDto::of)
+        .collect(Collectors.toList());
   }
 }
