c717927270febc1d44a8f2c7dacc9e5a4ecd96a3
Sangyups <feels4637@gmail.com>
2022-03-13 18:23:50 +0900

    feat: [WebController] 받아 온 dto를 모델 attribute에 추가

--- src/main/java/com/sangyups/springPractice/web/controller/WebController.java
+++ src/main/java/com/sangyups/springPractice/web/controller/WebController.java
@@ -1,13 +1,20 @@
 package com.sangyups.springPractice.web.controller;
 
+import com.sangyups.springPractice.posts.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class WebController {
 
+  private final PostsService postsService;
+
   @GetMapping("/")
-  public String main() {
+  public String main(Model model) {
+    model.addAttribute("posts", postsService.getPostsByRecent());
     return "index";
   }
 }
