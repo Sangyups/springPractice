0fb20c74d05be02ab9a658bd01fd75c6c441f110
Sangyups <feels4637@gmail.com>
2022-03-13 14:45:00 +0900

    test: [PostsRepositoryTest] 게시글 저장 및 불러오기 추가

--- src/test/java/com/sangyups/springPractice/posts/repository/PostsRepositoryTest.java
+++ src/test/java/com/sangyups/springPractice/posts/repository/PostsRepositoryTest.java
@@ -1,9 +1,14 @@
 package com.sangyups.springPractice.posts.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.sangyups.springPractice.posts.domain.Posts;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -11,16 +16,35 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @DataJpaTest
 class PostsRepositoryTest {
 
-  private final PostsRepository postsRepository;
-
-  public PostsRepositoryTest(
-      PostsRepository postsRepository) {
-    this.postsRepository = postsRepository;
-  }
+  @Autowired
+  private PostsRepository postsRepository;
 
   @AfterEach
   public void cleanUp() {
     postsRepository.deleteAll();
   }
 
+  @Test
+  void 게시글_저장_및_불러오기() {
+    // given
+    final String expectAuthor = "Sangyup";
+    final String expectTitle = "Title";
+    final String expectContent = "content";
+
+    postsRepository.save(Posts.builder()
+        .author(expectAuthor)
+        .title(expectTitle)
+        .content(expectContent)
+        .build());
+
+    // when
+    List<Posts> postsList = postsRepository.findAll();
+
+    // then
+    Posts post = postsList.get(0);
+
+    assertThat(post.getAuthor()).isEqualTo(expectAuthor);
+    assertThat(post.getTitle()).isEqualTo(expectTitle);
+    assertThat(post.getContent()).isEqualTo(expectContent);
+  }
 }
