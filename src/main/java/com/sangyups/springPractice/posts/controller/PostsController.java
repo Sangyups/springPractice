3d42efe9a19295fdaa3dce3630ca03ac56e429e5
Sangyups <feels4637@gmail.com>
2022-03-13 15:21:36 +0900

    feat: [PostsController] /posts postMapping 추가

--- src/main/java/com/sangyups/springPractice/posts/controller/PostsController.java
+++ src/main/java/com/sangyups/springPractice/posts/controller/PostsController.java
@@ -1,7 +1,11 @@
 package com.sangyups.springPractice.posts.controller;
 
+import com.sangyups.springPractice.posts.domain.dto.PostsRequestDto;
 import com.sangyups.springPractice.posts.repository.PostsRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +14,10 @@ public class PostsController {
 
   private final PostsRepository postsRepository;
 
-  
+  @PostMapping("/posts")
+  public ResponseEntity<Void> savePosts(@RequestBody PostsRequestDto postsRequestDto) {
+    postsRepository.save(postsRequestDto.toEntity());
+
+    return ResponseEntity.status(201).build();
+  }
 }
