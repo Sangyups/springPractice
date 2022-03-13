a9f654f9c9086b5f8dfd4507bcae6e265ae85c18
Sangyups <feels4637@gmail.com>
2022-03-13 16:40:51 +0900

    refactor: [PostsController] controller와 service 분리

--- src/main/java/com/sangyups/springPractice/posts/controller/PostsController.java
+++ src/main/java/com/sangyups/springPractice/posts/controller/PostsController.java
@@ -2,6 +2,7 @@ package com.sangyups.springPractice.posts.controller;
 
 import com.sangyups.springPractice.posts.domain.dto.PostsRequestDto;
 import com.sangyups.springPractice.posts.repository.PostsRepository;
+import com.sangyups.springPractice.posts.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PostsController {
 
-  private final PostsRepository postsRepository;
+  private final PostsService postsService;
 
   @PostMapping("/posts")
   public ResponseEntity<Void> savePosts(@RequestBody PostsRequestDto postsRequestDto) {
-    postsRepository.save(postsRequestDto.toEntity());
+    postsService.save(postsRequestDto);
 
     return ResponseEntity.status(201).build();
   }
