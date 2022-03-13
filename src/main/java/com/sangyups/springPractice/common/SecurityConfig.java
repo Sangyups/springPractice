3026613c02bb2c41d27964265250d25f186fd254
Sangyups <feels4637@gmail.com>
2022-03-13 15:32:09 +0900

    feat: [SecurityConfig] h2-console 확인용 설정 추가

--- src/main/java/com/sangyups/springPractice/common/SecurityConfig.java
+++ src/main/java/com/sangyups/springPractice/common/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.sangyups.springPractice.common;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -11,6 +12,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable()
         .authorizeRequests()
-        .antMatchers("/*/**").permitAll();
+        .antMatchers("/*/**").permitAll()
+        .and().headers().addHeaderWriter(
+            new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
   }
 }
